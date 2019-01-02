3bc4a90425b776c9aef91a0d0b2c389aa7e35bac
Henry <henry@reed.edu>
2019-01-01 18:31:03 -0800

    migrated out ATF to start in own repo

--- ATF/examples/hypaper.js
+++
@@ -1,4 +0,0 @@
-var paper_title = document.getElementById("title").innerText
-var head_title = document.createElement("title")
-head_title.innerText = paper_title
-document.getElementsByTagName("head")[0].appendChild(head_title)
--- ATF/readme.md
+++
@@ -1,7 +0,0 @@
-# Abstract Typesetting Framework
-
-A framework for custom transpilation to
-- HTML
-- LaTex
-- _more coming soon_
-
